1a9895a5aa5e8eff7c0793fe3c307aa4aa2382a7
snayyar00 <39316104+snayyar00@users.noreply.github.com>
2020-09-15 03:56:23 -0700

    video section in progress

---
+++ swipeforfun/src/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <video src="https://www.tiktok.com/@anji_t/video/6834195979168763141?lang=en"></video>
+      {/* videoFooter */}
+      {/* videoSidebar */}
+    </div>
+  );
+}
+
+export default Video;
